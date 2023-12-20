975dddd797ae779bdce78afaa5b9bcc244ab4b19
kakashi1210 <desmondaditya@gmail.com>
2023-12-21 02:08:54 +0530

    feat: added hero model, updated footer, navbar and updated links

--- apps/web/app/(marketing)/layout.tsx
+++ apps/web/app/(marketing)/layout.tsx
@@ -1,5 +1,4 @@
 import { NavBar } from "@/components/layout/navbar"
-import { SiteFooter } from "@/components/layout/site-footer"
 import { marketingConfig } from "@/config/marketing"
 import { getCurrentUser } from "@/lib/session"
 import { Suspense } from "react"
@@ -18,8 +17,7 @@ export default async function MarketingLayout({
       <Suspense fallback="...">
         <NavBar user={user} items={marketingConfig.mainNav} scroll={true} />
       </Suspense>
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
+      <main className="flex-1 checks-container">{children}</main>
     </div>
   )
 }
