2260aa9802a944f6394b185f7c4346d41309b59f
kunal00000 <kunalverma2468@gmail.com>
2023-12-27 02:43:35 +0530

    (chore): small dashboard changes

--- apps/web/app/dashboard/loading.tsx
+++ apps/web/app/dashboard/loading.tsx
@@ -1,17 +1,15 @@
-import { CardSkeleton } from "@/components/shared/card-skeleton"
-import { DashboardHeader } from "@/components/dashboard/header"
-import { DashboardShell } from "@/components/dashboard/shell"
-import { Button } from "@/components/ui/button"
+import { CardSkeleton } from '@/components/shared/card-skeleton'
+import { DashboardHeader } from '@/components/dashboard/header'
+import { DashboardShell } from '@/components/dashboard/shell'
+import { Button } from '@/components/ui/button'
 
 export default function DashboardLoading() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <Button>Fake button</Button>
+      <DashboardHeader heading='Dashboard' text='Create and manage projects.'>
+        <Button>Add Project</Button>
       </DashboardHeader>
-      <div className="divide-border-200 divide-y rounded-md border">
-        <CardSkeleton />
-        <CardSkeleton />
+      <div className='divide-border-200 divide-y rounded-md border'>
         <CardSkeleton />
         <CardSkeleton />
       </div>
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -5,7 +5,10 @@ import { getCurrentUser } from '@/lib/session'
 import { EmptyPlaceholder } from '@/components/shared/empty-placeholder'
 import { DashboardHeader } from '@/components/dashboard/header'
 import { DashboardShell } from '@/components/dashboard/shell'
-import { Button } from '@/components/ui/button'
+import { prisma } from '@/lib/db'
+import CreateButton, {
+  AddButton
+} from '@/components/dashboard/add-project-button'
 
 export const metadata = {
   title: 'Dashboard'
@@ -18,20 +21,35 @@ export default async function DashboardPage() {
     redirect(authOptions?.pages?.signIn || '/login')
   }
 
+  const userProjects = await prisma.project.findMany({
+    where: {
+      creatorId: user.id
+    }
+  })
+
   return (
     <DashboardShell>
-      <DashboardHeader heading='Panel' text='Create and manage content.'>
-        <Button>Fake button</Button>
+      <DashboardHeader heading='Dashboard' text='Create and manage projects.'>
+        <AddButton />
       </DashboardHeader>
       <div>
-        <EmptyPlaceholder>
-          <EmptyPlaceholder.Icon name='post' />
-          <EmptyPlaceholder.Title>No projects created</EmptyPlaceholder.Title>
-          <EmptyPlaceholder.Description>
-            You don&apos;t have any project yet. Start a project now.
-          </EmptyPlaceholder.Description>
-          <Button variant='outline'>Create project</Button>
-        </EmptyPlaceholder>
+        {userProjects.length > 0 ? (
+          <div>
+            Projects
+            {userProjects.map((project) => (
+              <div>{project.name}</div>
+            ))}
+          </div>
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Icon name='post' />
+            <EmptyPlaceholder.Title>No projects found</EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              You don&apos;t have any project yet. Start a project now.
+            </EmptyPlaceholder.Description>
+            <CreateButton />
+          </EmptyPlaceholder>
+        )}
       </div>
     </DashboardShell>
   )
