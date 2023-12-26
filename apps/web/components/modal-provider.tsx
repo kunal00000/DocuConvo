651031d22873b26e0dc228f20679cd5f731ff2cc
kunal00000 <kunalverma2468@gmail.com>
2023-12-27 02:46:38 +0530

    (feat): add project  modal form hook

--- apps/web/components/modal-provider.tsx
+++ apps/web/components/modal-provider.tsx
@@ -1,19 +1,21 @@
-"use client";
+'use client'
 
-import { SignInModal } from "@/components/layout/sign-in-modal";
-import { useMounted } from "@/hooks/use-mounted";
+import { SignInModal } from '@/components/layout/sign-in-modal'
+import { useMounted } from '@/hooks/use-mounted'
+import { AddProjectModal } from './dashboard/add-project-modal'
 
 export const ModalProvider = () => {
   const mounted = useMounted()
 
   if (!mounted) {
-    return null;
+    return null
   }
 
   return (
     <>
       <SignInModal />
+      <AddProjectModal />
       {/* add your own modals here... */}
     </>
-  );
-};
+  )
+}
---
+++ apps/web/hooks/use-modal.ts
@@ -0,0 +1,65 @@
+import { create } from 'zustand'
+
+interface useSignInModalStore {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+
+export const useSigninModal = create<useSignInModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false })
+}))
+
+interface useAddProjectModalStore {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+  currentStepIndex: number
+  nextStep: () => void
+  previousStep: () => void
+  goTo: (index: number) => void
+  isFirstStep: () => boolean
+  isLastStep: () => boolean
+}
+
+export const useAddProjectModal = create<useAddProjectModalStore>(
+  (set, get) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+
+    currentStepIndex: 0,
+
+    nextStep: () => {
+      const { currentStepIndex } = get()
+
+      if (currentStepIndex < 2) {
+        set({ currentStepIndex: currentStepIndex + 1 })
+      }
+    },
+
+    previousStep: () => {
+      const { currentStepIndex } = get()
+
+      if (currentStepIndex > 0) {
+        set({ currentStepIndex: currentStepIndex - 1 })
+      }
+    },
+
+    goTo: (index: number) => {
+      set({ currentStepIndex: index })
+    },
+
+    isFirstStep: () => {
+      const { currentStepIndex } = get()
+      return currentStepIndex === 0
+    },
+
+    isLastStep: () => {
+      const { currentStepIndex } = get()
+      return currentStepIndex === 2
+    }
+  })
+)
