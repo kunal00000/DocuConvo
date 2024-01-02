855bd894f8b70c417786a715593c039beac86cd7
kunal00000 <kunalverma2468@gmail.com>
2024-01-03 03:46:40 +0530

    (chore): remove docs app, ui package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,7 +6,7 @@ import('./env.mjs')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@docuconvo/core', '@docuconvo/database'],
+  transpilePackages: ['@docuconvo/database'],
   swcMinify: true,
   images: {
     remotePatterns: [
