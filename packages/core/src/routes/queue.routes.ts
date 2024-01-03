ca200eea7a0cdd8516c2b3fa60167c176c1ac380
kakashi1210 <desmondaditya@gmail.com>
2024-01-03 21:44:15 +0530

    feat: implemented job queue and added necessary TODO

---
+++ packages/core/src/routes/queue.routes.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { addToQueue } from "../controllers/addtoQueue.controllers";
+
+const queueRouter = express.Router()
+
+queueRouter.route('/queue').post(addToQueue)
+
+export default queueRouter
