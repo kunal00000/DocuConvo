c986da429e83c65fcf787ac72c9bb91bad830ee1
Junaid <onlyjunaid2002@gmail.com>
2024-01-06 16:12:50 +0530

    openai added for customized answer

--- packages/core/src/controllers/query.controllers.ts
+++ packages/core/src/controllers/query.controllers.ts
@@ -2,8 +2,10 @@ import type { Request, Response } from 'express'
 import { HuggingFaceInferenceEmbeddings } from 'langchain/embeddings/hf'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
-
+import { Configuration, OpenAIApi,ResponseTypes} from "openai-edge"
 import { Pinecone } from '@pinecone-database/pinecone'
+
+
 
 export const searchQuery = async (req: Request, res: Response) => {
   const {
@@ -15,11 +17,16 @@ export const searchQuery = async (req: Request, res: Response) => {
   } = req.body
 
   const searchQuery = req.query.q as string
+ 
   try {
     const pinecone = new Pinecone({
       apiKey: pineconeApiKey,
       environment: pineconeEnvironment
     })
+    const configuration = new Configuration({
+      apiKey: openaiApiKey,
+    })
+    const openai = new OpenAIApi(configuration)
 
     const pineconeIndex = await pinecone.Index(pineconeIndexName)
 
@@ -36,13 +43,30 @@ export const searchQuery = async (req: Request, res: Response) => {
     const results = await vectorStore.similaritySearch(searchQuery, 2, {
       project: projectId
     })
+    let contextText:string  = results[0].pageContent.replace(/<[^>]*>?/gm, '') + " " + results[1].pageContent.replace(/<[^>]*>?/gm, '')
 
-    // TODO: gpt interaction
+   
+
+//gpt 3.5 turbo
+   const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{
+      role: 'user',
+      content: getPrompt(searchQuery,contextText),
+    }],
+    max_tokens: 512,
+    temperature: 0
+  })
+  
+  const data = (await response.json()) as ResponseTypes["createChatCompletion"]
+  
+  const answer = data.choices[0].message
+
 
     return res.status(200).json({
       success: true,
-      message: 'Answer generated successfully.',
-      answer: results
+      message: 'Query successful.',
+      answer: answer?.content
     })
   } catch (err: any) {
     return res.status(404).json({
@@ -51,4 +75,26 @@ export const searchQuery = async (req: Request, res: Response) => {
       answer: null
     })
   }
+}
+
+const getPrompt = (query:string,context:string)=>{
+  return `
+  ${`
+    You are a very enthusiastic Organisation representative who loves
+    to help people! Given the following sections from the Organisation
+    documentation, answer the question using only that information,
+    outputted in markdown format. If you are unsure and the answer
+    is not explicitly written in the documentation, say
+    "Sorry, I don't know how to help with that."
+  `}
+
+  Context sections:
+  ${context}
+
+  Question: """
+  ${query}
+  """
+
+  Answer as markdown (including related code snippets if available):
+`
 }
