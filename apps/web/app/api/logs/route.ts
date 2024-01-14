96a6edacba66e7870435f90e8563987d5d908258
kunal00000 <kunalverma2468@gmail.com>
2024-01-14 09:51:08 +0530

    (chore): fetch previous logs

---
+++ apps/web/app/api/logs/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { prisma } from '@docuconvo/database'
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams
+    const projectId = searchParams.get('projectId')
+
+    if (!projectId) {
+      return NextResponse.json({ error: 'No projectId provided' })
+    }
+
+    const logs = await prisma.logMessage.findMany({
+      where: {
+        projectId: projectId!
+      }
+    })
+
+    return NextResponse.json({ logs })
+  } catch (error) {
+    return NextResponse.json({ error })
+  }
+}
