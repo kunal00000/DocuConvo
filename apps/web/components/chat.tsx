1ddba1a3556ddb28edf053aae0f372d5eaa29276
kunal00000 <kunalverma2468@gmail.com>
2024-03-15 21:52:56 +0530

    streaming chat responses

--- apps/web/components/chat.tsx
+++ apps/web/components/chat.tsx
@@ -1,28 +1,54 @@
 'use client'
 
+// import { useChat } from '@/hooks/use-chat'
+import { useChat } from 'ai/react'
 import ReactMarkdown from 'react-markdown'
-
-import { useChat } from '@/hooks/use-chat'
 
 import { Icons } from './shared/icons'
 
 const Chat = ({ project }) => {
+  // const { messages, input, handleInputChange, handleSubmit, isLoading } =
+  //   useChat({ project })
+
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
-    useChat({ project })
+    useChat({
+      api: 'http://localhost:3000/api/query',
+      headers: { Authorization: `Bearer ${project.docuconvo_key}` }
+    })
 
   return (
-    <div className='flex p-10 h-[500px] flex-col'>
-      <div className='flex-1 space-y-6 overflow-y-auto rounded-xl bg-slate-200 p-4 text-sm leading-6 text-slate-900 shadow-sm dark:bg-slate-900 dark:text-slate-300 sm:text-base sm:leading-7'>
+    <div className='flex p-10 h-[600px] flex-col'>
+      <div className='flex-1 space-y-6 overflow-y-auto rounded-xl bg-slate-100 p-4 text-sm leading-6 text-slate-900 shadow-sm dark:bg-slate-900 dark:text-slate-300 sm:text-base sm:leading-7'>
         {messages.map((m, index) => (
-          <div key={index}>
-            {m.role === 'ai' ? (
+          <div key={index} className='w-full'>
+            {m.role === 'assistant' ? (
               <div className='flex items-start'>
-                <img
-                  className='mr-2 h-8 w-8 rounded-full'
-                  src='https://dummyimage.com/128x128/354ea1/ffffff&text=G'
-                />
-                <div className='flex min-h-[85px] rounded-b-xl rounded-tr-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl'>
-                  <ReactMarkdown>{m.content}</ReactMarkdown>
+                <div className='px-3 py-2.5 bg-white'>
+                  <div className='m-auto flex max-w-4xl items-start space-x-3'>
+                    <span className='flex overflow-hidden rounded-full bg-white border border-blue-950 w-8 h-8'>
+                      <div className='m-auto'>
+                        <svg
+                          stroke='none'
+                          fill='black'
+                          strokeWidth='1.5'
+                          viewBox='0 0 24 24'
+                          aria-hidden='true'
+                          height='20'
+                          width='20'
+                          xmlns='http://www.w3.org/2000/svg'>
+                          <path
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            d='M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z'></path>
+                        </svg>
+                      </div>
+                    </span>
+                    <div className='w-full min-w-0 text-sm sm:text-base'>
+                      <div className='prose prose-stone prose-sm sm:prose-base prose-pre:rounded-md prose-p:whitespace-pre-wrap prose-p:break-words prose-p:leading-7 prose-pre:bg-[#282c34] w-full max-w-full flex-1 leading-6'>
+                        <ReactMarkdown>{m.content}</ReactMarkdown>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className='ml-2 mt-1 flex flex-col-reverse gap-2 text-slate-500 sm:flex-row'>
                   <button className='hover:text-blue-600'>
@@ -56,13 +82,27 @@ const Chat = ({ project }) => {
                 </div>
               </div>
             ) : (
-              <div className='flex flex-row-reverse items-start'>
-                <img
-                  className='ml-2 h-8 w-8 rounded-full'
-                  src='https://dummyimage.com/128x128/363536/ffffff&text=J'
-                />
-                <div className='flex rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:max-w-md md:max-w-2xl'>
-                  <p>{m.content}</p>
+              <div className='bg-white px-3 py-2.5'>
+                <div className='mx-auto flex max-w-5xl items-start space-x-3'>
+                  <span className='flex overflow-hidden rounded-full bg-white border border-blue-950 w-8 h-8'>
+                    <div className='m-auto'>
+                      <svg
+                        stroke='none'
+                        fill='black'
+                        strokeWidth='0'
+                        viewBox='0 0 16 16'
+                        height='20'
+                        width='20'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z'></path>
+                      </svg>
+                    </div>
+                  </span>
+                  <div className='w-full min-w-0 text-sm sm:text-base'>
+                    <div className='prose prose-stone prose-sm sm:prose-base prose-pre:rounded-md prose-p:whitespace-pre-wrap prose-p:break-words prose-p:leading-7 prose-pre:bg-[#282c34] w-full max-w-full flex-1 leading-6'>
+                      <p>{m.content}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             )}
@@ -74,7 +114,7 @@ const Chat = ({ project }) => {
           </div>
         )}
       </div>
-      {/* Prompt message input */}
+
       <form className='mt-2' onSubmit={handleSubmit}>
         <label htmlFor='chat-input' className='sr-only'>
           Enter your prompt
